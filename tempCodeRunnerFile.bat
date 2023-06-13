281fb48956abb9454e14e02f7812fe51b9554072
Wunyje <wunyje@gmail.com>
2023-06-13 21:02:38 +0800

    update

--- tempCodeRunnerFile.bat
+++ tempCodeRunnerFile.bat
@@ -1,42 +1,42 @@
 adb connect 192.168.8.191
-rem WiFi连接手机
+rem WiFi�����ֻ�
 
 adb devices
-rem 查看adb连接手机设备
+rem �鿴adb�����ֻ��豸
 
 adb shell input keyevent 224
-rem 点亮屏幕
+rem ������Ļ
 
 adb shell am start -n com.alibaba.android.rimet/com.alibaba.android.rimet.biz.LaunchHomeActivity
-rem 找到钉钉应用，并打开。
+rem �ҵ�����Ӧ�ã����򿪡�
 ping /n 6 /w 1000 localhost > nul
-rem 等待2秒%
+rem �ȴ�2��%
 
 adb shell input tap 550 2200
-rem 进入钉钉后，点击:【开源网安】
+rem ���붤���󣬵��:����Դ������
 ping /n 20 /w 1000 localhost > nul
 
 adb shell input tap 125 1415
-rem 点击【考勤打卡】
+rem ��������ڴ򿨡�
 ping /n 21 /w 1000 localhost > nul
 
 adb shell input tap 530 1360
-rem 点击【上班打卡，下班打卡】
+rem ������ϰ�򿨣��°�򿨡�
 ping /n 5 /w 1000 localhost > nul
 
 adb shell screencap -p /sdcard/check_res.png
 ping /n 2 /w 1000 localhost > nul
 adb pull /sdcard/check_res.png
 ping /n 2 /w 1000 localhost > nul
-rem 截图成功打卡图片到电脑
+rem ��ͼ�ɹ���ͼƬ������
 
 
 adb shell am force-stop com.alibaba.android.rimet
-rem 关闭钉钉
+rem �رն���
 ping /n 2 /w 1000 localhost > nul
 
 adb shell input keyevent 26
-rem 关闭手机屏幕。
+rem �ر��ֻ���Ļ��
 ping /n 2 /w 1000 localhost > nul
 
 check_res.png
