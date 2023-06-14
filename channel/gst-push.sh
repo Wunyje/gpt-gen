6dc34557f46923aaa8ae42bce9d059debebe7067
Wunyje <wunyje@gmail.com>
2023-06-14 16:58:11 +0800

    Update

--- channel/gst-push.sh
+++ channel/gst-push.sh
@@ -1,1 +1,1 @@
-GET_DEBUG=1 gst-launch-1.0 v4l2src device=/dev/video31 ! video/x-raw,width=640,height=480 ! x264enc pass=qual quantizer=25 tune=zerolatency ! flvmux streamable=true ! rtmpsink location="rtmp://192.168.8.35:1935/test"
+gst-launch-1.0 -v videotestsrc ! x264enc ! rtph264pay ! udpsink host=192.168.8.35 port=5000
