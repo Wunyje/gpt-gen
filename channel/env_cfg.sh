587c8fe81067a5a2862e81094a2773daac4debcc
Wunyje <wunyje@gmail.com>
2023-06-15 15:08:49 +0800

    Update

--- channel/env_cfg.sh
+++
@@ -1,9 +0,0 @@
-cd ~Downloads/
-# mpp cfg
-git clone https://gitcode.net/mirrors/rockchip-linux/mpp.git
-cd mpp
-
-
-
-# install 6ft vpn
-curl -sL https://dl.6ftproxy.com/cli/v1.1.0/install.sh | sudo bash -
--- channel/mpp-build.sh
+++
@@ -1,2 +0,0 @@
-DEB_BUILD_OPTIONS="parallel=4 nocheck" dpkg-buildpackage -a arm64
-cmake -DRKPLATFORM=ON -DHAVE_DRM=ON && make
