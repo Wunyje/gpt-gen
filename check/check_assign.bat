3c4591637b121388b9794796f661e5f1fbf64cf1
Wunyje <wunyje@gmail.com>
2023-06-15 12:27:13 +0800

    Update

--- check/check_assign.bat
+++ check/check_assign.bat
@@ -4,7 +4,7 @@ set /a rand_minute=%RANDOM% %% 50 + 10
 schtasks /create /tn check_test /st 11:%rand_minute% /sc once /tr %~dp0check_test.bat  < Y.txt
 
 set /a rand_minute=%RANDOM% %% 50 + 10 
-schtasks /create /tn check_in_task /st 07:%rand_minute% /sc DAILY /tr "check_in.bat"  < Y.txt
+schtasks /create /tn check_in_task /st 07:%rand_minute% /sc once /tr %~dp0check_in.bat  < Y.txt
 
 set /a rand_minute=%RANDOM% %% 50 + 10  
-schtasks /create /tn check_out_task /st 21:%rand_minute% /sc DAILY /tr "check_out.bat"  < Y.txt
+schtasks /create /tn check_out_task /st 21:%rand_minute% /sc once /tr %~dp0check_out.bat  < Y.txt
