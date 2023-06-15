e049aa3f72d6c62c4cfe9c0159876c925cd65a9b
Wunyje <wunyje@gmail.com>
2023-06-15 14:52:19 +0800

    check assign done

--- check/check_assign.bat
+++ check/check_assign.bat
@@ -14,3 +14,13 @@ schtasks /create /tn check_in_task /st 07:%rand_minute%:%rand_sec% /sc once /tr 
 set /a rand_minute=%RANDOM% %% 50 + 10  
 set /a rand_sec=%RANDOM% %% 50 + 10 
 schtasks /create /tn check_out_task /st 21:%rand_minute%:%rand_sec% /sc once /tr %~dp0check_out.bat  < Y.txt
+
+adb connect 192.168.8.191
+adb shell input keyevent 224
+adb shell screencap -p /sdcard/check_assign.png
+adb pull /sdcard/check_assign.png
+adb shell input keyevent 26
+
+git add --all
+git commit -m "check assign done"
+git push
