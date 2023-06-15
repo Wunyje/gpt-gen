3cb4ba798ab8651b73e3573099664b163567dfa9
WT <244001428@qq.com>
2023-06-15 10:38:21 +0800

    update

--- channel/gst-play-usb.sh
+++ channel/gst-play-usb.sh
@@ -1,42 +1,42 @@
-gst-launch-1.0 v4l2src device=/dev/video1 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video2 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video3 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video4 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video5 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video6 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video7 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video8 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video9 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video10 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video11 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video12 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video13 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video14 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video15 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video16 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video17 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video18 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video19 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video20 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video21 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video1 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video2 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video3 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video4 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video5 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video6 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video7 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video8 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video9 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video10 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video11 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video12 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video13 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video14 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video15 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video16 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video17 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video18 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video19 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video20 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video21 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
 # gst-launch-1.0 v4l2src device=/dev/video22 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
 # gst-launch-1.0 v4l2src device=/dev/video23 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video24 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video25 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video26 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video27 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video28 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video29 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video30 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video24 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video25 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video26 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video27 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video28 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video29 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video30 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
 # gst-launch-1.0 v4l2src device=/dev/video31 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
 # gst-launch-1.0 v4l2src device=/dev/video32 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video33 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video34 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video35 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video36 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video37 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video38 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video39 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video40 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video41 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
-gst-launch-1.0 v4l2src device=/dev/video42 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video33 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video34 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video35 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video36 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video37 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video38 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video39 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video40 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video41 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
+# gst-launch-1.0 v4l2src device=/dev/video42 ! videoconvert! videoscale ! video/x-raw, width=800, height=1080 ! autovideosink -v
