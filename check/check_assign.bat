9abdcf3ddf72c1967f4fa758451920b4f67b5181
Wunyje <wunyje@gmail.com>
2023-06-27 19:22:13 +0800

    Update

--- check/check_assign.bat
+++ check/check_assign.bat
@@ -8,13 +8,13 @@ cd /d %~dp0
 :: schtasks /create /tn check_test /st 10:00:00 /sc once /tr %~dp0check_test.bat  < Y.txt
 :: 11:%rand_minute%:%rand_sec%
 
-set /a rand_minute=%RANDOM% %% 50 + 10 
-set /a rand_sec=%RANDOM% %% 50 + 10 
-schtasks /create /tn check_in_task /st 07:%rand_minute%:%rand_sec% /sc once /tr %~dp0check_in.bat  < Y.txt
+set /a check_in_rand_minute=%RANDOM% %% 50 + 10 
+set /a check_in_rand_sec=%RANDOM% %% 50 + 10 
+schtasks /create /tn check_in_task /st 07:%check_in_rand_minute%:%check_in_rand_sec% /sc once /tr %~dp0check_in.bat  < Y.txt
 
-set /a rand_minute=%RANDOM% %% 50 + 10  
-set /a rand_sec=%RANDOM% %% 50 + 10 
-schtasks /create /tn check_out_task /st 21:%rand_minute%:%rand_sec% /sc once /tr %~dp0check_out.bat  < Y.txt
+set /a check_out_rand_minute=%RANDOM% %% 50 + 10  
+set /a check_out_rand_sec=%RANDOM% %% 50 + 10 
+schtasks /create /tn check_out_task /st 21:%check_out_rand_minute%:%check_out_rand_sec% /sc once /tr %~dp0check_out.bat  < Y.txt
 
 adb connect 192.168.8.191
 adb devices
