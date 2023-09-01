9724671ecafd72ba8257c14ac493542ecde01040
Wunyje <wunyje@gmail.com>
2023-09-01 17:10:29 +0800

    check out done in 2023/09/01 周五

--- check/check_assign.bat
+++ check/check_assign.bat
@@ -19,12 +19,12 @@ schtasks /create /tn check_out_task /st 21:%check_out_rand_minute%:%check_out_ra
 
 adb connect 192.168.8.191
 adb devices
-adb shell input keyevent 224
-adb shell screencap -p /sdcard/check_assign.png
+adb -s 192.168.8.191:5555 shell input keyevent 224
+adb  -s 192.168.8.191:5555 shell screencap -p /sdcard/check_assign.png
 ping /n 2 /w 1000 localhost > nul
-adb pull /sdcard/check_assign.png
+adb  -s 192.168.8.191:5555 pull /sdcard/check_assign.png
 ping /n 2 /w 1000 localhost > nul
-adb shell input keyevent 26
+adb  -s 192.168.8.191:5555 shell input keyevent 26
 
 git add --all
 git commit -m "07:%check_in_rand_minute%:%check_in_rand_sec%  21:%check_out_rand_minute%:%check_out_rand_sec%"
